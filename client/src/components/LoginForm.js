2880ef2cb6d69b34f73b2ca7a16e9189ef96e93e
allanj <allan.jard@aicorporation.com>
2018-05-10 14:26:22 +0100

    LoginForm added

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,44 +3,23 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
     render() {
         return (
-
-  <div className="row">
-  <form className="col s12">
-    <div className="row">
-      <div className="input-field col s6">
-        <label for="first_name">First Name</label>
+          
+          <form class="form-signin">
+      <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus />
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required />
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
       </div>
-      <div className="input-field col s6">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
     
-        <label for="last_name">Last Name</label>
-      </div>
-    </div>
-    <div className="row">
-      <div className="input-field col s12">
-        <label for="disabled">Disabled</label>
-      </div>
-    </div>
-    <div className="row">
-      <div className="input-field col s12">
-        <label for="password">Password</label>
-      </div>
-    </div>
-    <div className="row">
-      <div className="input-field col s12">
-        <label for="email">Email</label>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col s12">
-        This is an inline input field:
-        <div className="input-field inline">
-          <label for="email" data-error="wrong" data-success="right">Email</label>
-        </div>
-      </div>
-    </div>
-  </form>
-</div>
-      
         );
     }
 }
